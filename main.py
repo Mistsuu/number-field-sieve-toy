6db51f1f2b2a8e50b124744a7626bc8a7b18aa49
Mistsu <vietanhtran12@gmail.com>
2024-01-16 22:05:23 +0700

    Refactoring

--- main.py
+++ main.py
@@ -7,25 +7,34 @@ from sieve            import find_algebraic_and_rational_smooths
 from calc             import algebraic_legendre_symbols
 
 def factor(
-    args: Args
-) -> None:
-    N = ZZ(args.N)
-    d = ZZ(args.d)
-    boundZ = args.boundZ
-    boundA = args.boundA
-    boundQ = args.boundQ
-    sieve_a_bound = (args.lbsieve_a, args.ubsieve_a)
-
+    N: ZZ,
+    d: ZZ,
+    boundZ: int,
+    boundA: int,
+    boundQ: int,
+    sieve_a_bound: tuple[int, int]
+) -> int:
+    # Find m, f such that
+    # f(m) == 0 mod N
+    # and deg(f) == d.
     f, m = polysearch(N, d)
 
+    # Find 3 types of
+    # bases
     rbases = find_rational_factor_bases(boundZ)
     abases = find_algebraic_factor_bases(f, boundA)
     qbases = find_quadratic_character_bases(f, boundA, boundQ)
 
-    smooth_candidates_info = SortedDict({})
+    # Find pairs (a,b)
+    # such that a+bm
+    # is smooth in ZZ,
+    # a+bO is smooth in
+    # Z[O].
     sieve_b = 1
-
-    while len(smooth_candidates_info) <= 1 + len(rbases) + len(abases) + len(qbases):
+    smooth_candidates_info = SortedDict({})
+    target_ncandidates = 1 + len(rbases) + len(abases) + len(qbases)
+    
+    while len(smooth_candidates_info) <= target_ncandidates:
         smooths, rexps, aexps = find_algebraic_and_rational_smooths(
                                     rbases,
                                     abases,
@@ -50,8 +59,22 @@ def factor(
         if sieve_b > 0:
             sieve_b += 1
 
+def main(
+    args: Args
+) -> None:
+    N = ZZ(args.N)
+    d = ZZ(args.d)
+    boundZ = args.boundZ
+    boundA = args.boundA
+    boundQ = args.boundQ
+    sieve_a_bound = (args.lbsieve_a, args.ubsieve_a)
+
+    print(f'[i] Factoring {N=}...')
+    p = factor(N, d, boundZ, boundA, boundQ, sieve_a_bound)
+    print(f'[i] Found {p=}!')
+
 if __name__ == '__main__':
     args = get_args()
     sanitize_args(args)
     check_args(args)
-    factor(args)
+    main(args)
